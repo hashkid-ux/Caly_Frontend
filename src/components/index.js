4b7a128aa799fcd30f14b45c6f66013765e17c0d
hashkid-ux <hashkid674@gmail.com>
2025-11-28 13:31:03 +0530

    upin2

---
+++ src/components/index.js
@@ -0,0 +1,38 @@
+// UI Components
+export { default as Breadcrumb } from './Breadcrumb';
+export { default as PageHeader } from './PageHeader';
+export { default as UserMenu } from './UserMenu';
+export { default as Sidebar } from './Sidebar';
+export { default as MobileNavigation } from './MobileNavigation';
+export { default as ResponsiveLayout } from './ResponsiveLayout';
+
+// Data Components
+export { default as DataTable } from './DataTable';
+export { default as Pagination } from './Pagination';
+export { default as SearchBar } from './SearchBar';
+
+// Form Components
+export { TextInput, PasswordInput, Textarea, Select, Checkbox, RadioGroup } from './FormInputs';
+export { Form, FormGroup, FormSection, FormActions } from './Form';
+
+// Feedback Components
+export { Toast, ToastContainer, useToast } from './Toast';
+export { default as Modal } from './Modal';
+export { Skeleton, TableSkeleton, CardSkeleton, TextSkeleton } from './LoadingSkeleton';
+export { default as EmptyState } from './EmptyState';
+
+// Display Components
+export { Badge, BadgeGroup } from './Badge';
+export { default as Card } from './Card';
+export { default as Accordion } from './Accordion';
+export { default as Button } from './Button';
+
+// Localization & Theme
+export { default as LanguageSwitcher } from './LanguageSwitcher';
+export { default as ThemeSwitcher } from './ThemeSwitcher';
+export { default as AccessibilityChecklist } from './AccessibilityChecklist';
+
+// Layout
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as OnboardingGuard } from './OnboardingGuard';
+export { default as ErrorBoundary } from './ErrorBoundary';
