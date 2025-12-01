be950b9a3fa238ab968c1633b5f2146495c0cf0a
hashkid-ux <hashkid674@gmail.com>
2025-12-01 09:57:47 +0530

    updatte fix

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,7 +4,7 @@
  */
 
 import { useContext } from 'react';
-import AuthContext from '../context/AuthContext';
+import { AuthContext } from '../context/AuthContext';
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
